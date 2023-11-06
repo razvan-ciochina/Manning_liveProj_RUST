aec0d0a7e8fcb4b208c37c0770e88a6ccf4842eb
Razvan-Adrian Ciochina <razvan.ciochina@gmail.com>
2023-11-06 10:43:17 +0200

    Async-std tests

--- 01-DataStreamingAsyncRust/src/main.rs
+++ 01-DataStreamingAsyncRust/src/main.rs
@@ -246,8 +246,8 @@ mod tests {
     #![allow(non_snake_case)]
     use super::*;
 
-    #[test]
-    fn test_PriceDifference_calculate() {
+    #[async_std::test]
+    async fn test_PriceDifference_calculate() {
         task::block_on(async {
             let signal = PriceDifference {};
             assert_eq!(signal.calculate(&[]).await, None);
@@ -266,9 +266,8 @@ mod tests {
         });
     }
 
-    #[test]
-    fn test_MinPrice_calculate() {
-        task::block_on(async {
+    #[async_std::test]
+    async fn test_MinPrice_calculate() {
             let signal = MinPrice {};
             assert_eq!(signal.calculate(&[]).await, None);
             assert_eq!(signal.calculate(&[1.0]).await, Some(1.0));
@@ -283,45 +282,40 @@ mod tests {
                 signal.calculate(&[0.0, 3.0, 5.0, 6.0, 1.0, 2.0, 1.0]).await,
                 Some(0.0)
             );
-        });
     }
 
-    #[test]
-    fn test_MaxPrice_calculate() {
-        task::block_on(async {
-            let signal = MaxPrice {};
-            assert_eq!(signal.calculate(&[]).await, None);
-            assert_eq!(signal.calculate(&[1.0]).await, Some(1.0));
-            assert_eq!(signal.calculate(&[1.0, 0.0]).await, Some(1.0));
-            assert_eq!(
-                signal
-                    .calculate(&[2.0, 3.0, 5.0, 6.0, 1.0, 2.0, 10.0])
-                    .await,
-                Some(10.0)
-            );
-            assert_eq!(
-                signal.calculate(&[0.0, 3.0, 5.0, 6.0, 1.0, 2.0, 1.0]).await,
-                Some(6.0)
-            );
-        });
+    #[async_std::test]
+    async fn test_MaxPrice_calculate() {
+        let signal = MaxPrice {};
+        assert_eq!(signal.calculate(&[]).await, None);
+        assert_eq!(signal.calculate(&[1.0]).await, Some(1.0));
+        assert_eq!(signal.calculate(&[1.0, 0.0]).await, Some(1.0));
+        assert_eq!(
+            signal
+                .calculate(&[2.0, 3.0, 5.0, 6.0, 1.0, 2.0, 10.0])
+                .await,
+            Some(10.0)
+        );
+        assert_eq!(
+            signal.calculate(&[0.0, 3.0, 5.0, 6.0, 1.0, 2.0, 1.0]).await,
+            Some(6.0)
+        );
     }
 
-    #[test]
-    fn test_WindowedSMA_calculate() {
-        task::block_on(async {
-            let series = vec![2.0, 4.5, 5.3, 6.5, 4.7];
+    #[async_std::test]
+    async fn test_WindowedSMA_calculate() {
+        let series = vec![2.0, 4.5, 5.3, 6.5, 4.7];
 
-            let signal = WindowedSMA { window_size: 3 };
-            assert_eq!(
-                signal.calculate(&series).await,
-                Some(vec![3.9333333333333336, 5.433333333333334, 5.5])
-            );
+        let signal = WindowedSMA { window_size: 3 };
+        assert_eq!(
+            signal.calculate(&series).await,
+            Some(vec![3.9333333333333336, 5.433333333333334, 5.5])
+        );
 
-            let signal = WindowedSMA { window_size: 5 };
-            assert_eq!(signal.calculate(&series).await, Some(vec![4.6]));
+        let signal = WindowedSMA { window_size: 5 };
+        assert_eq!(signal.calculate(&series).await, Some(vec![4.6]));
 
-            let signal = WindowedSMA { window_size: 10 };
-            assert_eq!(signal.calculate(&series).await, Some(vec![]));
-        });
+        let signal = WindowedSMA { window_size: 10 };
+        assert_eq!(signal.calculate(&series).await, Some(vec![]));
     }
 }
